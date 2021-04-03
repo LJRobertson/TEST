349d0b01cff99c5d6fa59bcf1fb1c8427c0fea0d
unknown <lindseyjrobertson@gmail.com>
2021-04-02 20:21:27 -0400

    initial commit

---
+++ FixGitMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(FixGitMVC.Startup))]
+namespace FixGitMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
